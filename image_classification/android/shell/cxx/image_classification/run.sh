05dd6255dd3965f3837e8d11f2d643fb521111d5
zhangyue66 <zhangyue66@baidu.com>
2024-08-27 15:45:04 +0800

    opt

--- image_classification/android/shell/cxx/image_classification/run.sh
+++ image_classification/android/shell/cxx/image_classification/run.sh
@@ -7,8 +7,8 @@ if [ -n "$1" ]; then
 fi
 
 if ! echo "$MODEL_LIST" | grep -qw "$MODEL_NAME"; then
-  echo "Supported model: ${MODEL_LIST}"
-  echo "$MODEL_NAME is not in the support list. Exiting."
+  echo "Supported models: ${MODEL_LIST}"
+  echo "$MODEL_NAME is not in the supported models. Now exiting."
   exit 1
 fi
 
@@ -20,11 +20,7 @@ PADDLE_LITE_DIR="$(pwd)/../../../../../libs/android/cxx"
 OPENCV_LITE_DIR="$(pwd)/../../../../../libs/android/opencv4.1.0"
 ASSETS_DIR="$(pwd)/../../../../assets"
 ADB_DIR="/data/local/tmp/image_classify"
-ARM_ABI=arm64-v8a # arm64-v8a or armeabi-v7a
-
-if [ -n "$2" ]; then
-  ARM_ABI="$2"
-fi
+ARM_ABI="${2:-arm64-v8a}" # arm64-v8a or armeabi-v7a
 
 if [ ! -d "${ASSETS_DIR}/models/${MODEL_NAME}" ];then
   echo "Model $MODEL_NAME not found! "
@@ -50,8 +46,8 @@ echo "--run model on cpu---"
 adb shell "cd ${ADB_DIR} \
            && chmod +x ./image_classification \
            && export LD_LIBRARY_PATH=${ADB_DIR}:${LD_LIBRARY_PATH} \
-           &&  ./image_classification \
-               ./models/${MODEL_NAME}/model.nb \
+           && ./image_classification \
+               ./models/\"${MODEL_NAME}\"/model.nb \
                ./images/tabby_cat.jpg \
                ./labels/labels.txt \
                3 224 224 \
